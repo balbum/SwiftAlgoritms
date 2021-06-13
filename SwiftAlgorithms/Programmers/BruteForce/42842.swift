8339dc0fc1d04e5e201416d2c2be8b94f6312d33
Brian Park <balbvm@gmail.com>
2021-06-13 23:23:40 +0900

    주석 추가

--- SwiftAlgorithms/Programmers/BruteForce/42842.swift
+++ SwiftAlgorithms/Programmers/BruteForce/42842.swift
@@ -11,11 +11,15 @@
 import Foundation
 
 func solution42842(_ brown: Int, _ yellow: Int) -> [Int] {
+    /// 아래 조건이 성립 할 경우
+    /// yellow == x × y
+    /// brown == ( x + 2 ) × 2 + y × 2
     for i in 1...(yellow == 1 ? 2 : yellow/2) {
         if yellow % i == 0 {
             let w = yellow / i
             let h = i
             if 2*(w+2) + 2*h == brown {
+                /// yellow 보다 가로, 세로 각각 +2
                 return [w+2, h+2]
             }
         }
